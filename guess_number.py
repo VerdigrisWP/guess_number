1ab0e552862f35bb8f607da400fe8ddf8ddec6f3
Verdigris Chang <wpccj64@gmail.com>
2019-02-10 20:09:52 +0800

    user set range

--- guess_number.py
+++ guess_number.py
@@ -1,20 +1,24 @@
 #猜數字
 import random
-nu = random.randint(1, 100)
+x = int(input('請輸入猜測範圍最小值'))
+y = int(input('請輸入猜測範圍最大值'))
+r_num = random.randint(x, y)
 count = 0
 while True:
-	input_nu = input('猜一個 1-100 的整數')
-	input_nu = int(input_nu)
+	print('你設定的範圍是', x, '-', y)
+	num = int(input('猜一個範圍內的整數'))
 	count += 1 # count = count + 1
-	if input_nu == nu:
+	if num == r_num:
 		print('恭喜你猜中了!')
 		print('你在第', count, '次猜中')
 		break
-	elif input_nu == 999:
-			print(nu)
+	elif num == 999:
+			print(r_num)
 	else:
-		if input_nu > nu:
-			print('你猜的數次太大，再猜一次')
-		else:
-			print('你猜的數次太小，再猜一次')
+		if num > y or num < x:
+			print('你猜的數字超過範圍')
+		elif num > r_num:
+			print('你猜的數字太大，再猜一次')
+		elif num < r_num:
+			print('你猜的數字太小，再猜一次')
 	print('這是你猜的第', count, '次')
